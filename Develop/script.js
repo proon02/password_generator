92f8125a7d505d695199a4df51d2c3df4eac4ffd
Peter Rooney <proon02@hotmail.com>
2020-09-03 13:54:57 +1000

    adding changes

--- Develop/script.js
+++ Develop/script.js
@@ -5,10 +5,9 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
+var caseArray=[];
 var specialCharacters = [
   '@',
   '%',
@@ -97,36 +96,46 @@ var upperCasedCharacters = [
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-  if (generateBtn) {
-  var useLowerCase = prompt("Do you want to include lowercase characters?")
+if (generateBtn) {
+var useLowerCase = confirm("Do you want to include lowercase characters?")
 
-var useUpperCase = prompt("Do you want to include uppercase characters?")
+var useUpperCase = confirm("Do you want to include uppercase characters?")
 
-var useNumeric = prompt("Do you want to include special characters?")
+var useNumeric = confirm("Do you want to include special characters?")
 
-useSpecial = prompt("Do you want to include number characters?")
+var useSpecial = confirm("Do you want to include number characters?")
 }
-let passwordLength=parseInt(prompt("How many characters do you want the password to be? The password cannot be less than 8 or more than 128 characters."));
+while (useLowerCase == false && useUpperCase == false && useNumeric == false && useSpecial == false) {
+  alert("Please select one type of character");
+  useLowerCase=confirm("Do you want to include lowercase characters?");
+  useUpperCase=confirm("Do you want to include uppercase characters?");
+  useNumeric=confirm("Do you want to include numeric characters?");
+  useSpecial=confirm("Do you want to include special characters?");
+}
+
+var passwordLength=parseInt(prompt("How many characters do you want the password to be? The password cannot be less than 8 or more than 128 characters."));
 while(passwordLength < 8 || passwordLength > 128 || typeof(passwordLength) != "number" || passwordLength === NaN || passwordLength === null) {
     alert("Please choose a number that is more than 8 and less than 128 characters");
     passwordLength=parseInt(prompt("How many characters do you want the password to be? The password cannot be less than 8 or more than 128 characters."));
 }
 
 if (useLowerCase==true){
-  caseArray.push(lowerCaseChar);
-
+  caseArray.push(lowerCasedCharacters)
 }
 if (useUpperCase==true){
-  caseArray.push(upperCaseChar)
+  caseArray.push(upperCasedCharacters)
 
 }
 if (useNumeric==true){
-  caseArray.push(numericChar)
+  caseArray.push(numericCharacters)
   
 }
 if (useSpecial==true){
-  caseArray.push(specialChar)
+  caseArray.push(specialCharacters)
 }
+
+//// a loop is run for the length of the password
+
 for(let i=0;i<passwordLength;i++){
 
   let randomCharArrayNum;
@@ -143,27 +152,21 @@ for(let i=0;i<passwordLength;i++){
   selectedCharArray=caseArray[randomCharArrayNum]; //variable that stores selected array from case array
   
   //console.log("The array has " + caseArray.length + " entries"); //testing length of caseArray
-  //console.log(randomCharArrayNum); //testing random number generated based on length of caseArray
+  console.log(randomCharArrayNum); //testing random number generated based on length of caseArray
 
   randomCharNum=Math.floor(Math.random()*selectedCharArray.length);// variable that stores random number based on length of selected array
 
-  // console.log("The selected Array is the " + selectedCharArray); //testing array chosen
-  // console.log("The selected random index is " + randomCharNum); //testing random number generation based on chosen array length 
+  console.log("The selected Array is the " + selectedCharArray); //testing array chosen
+  console.log("The selected random index is " + randomCharNum); //testing random number generation based on chosen array length 
 
   randomChar=selectedCharArray[randomCharNum];
-  //console.log(randomChar);
-  // console.log("Your random character array num is " + randomCharArrayNum + " and your random character number is " + randomCharNum);
+  console.log(randomChar);
+  console.log("Your random character array num is " + randomCharArrayNum + " and your random character number is " + randomCharNum);
 
   // now, need to accumulate the string
-  passwordString+=randomChar;
+  password+=randomChar;
   //return passwordString;
   
 }
-//console.log("Your password is " + passwordString);
-//alert("Your password is " + passwordString);
-passwordEntry.textContent=passwordString;    
-}
-
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
-
+console.log("Your password is " + password);
+alert("Your password is " + password);
