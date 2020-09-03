37844719a9a91189c9e133343af1852d248e0cc1
Peter Rooney <proon02@hotmail.com>
2020-09-03 11:04:29 +1000

    adding to github

---
+++ Develop/script.js
@@ -0,0 +1,169 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+var specialCharacters = [
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.'
+]; 
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+var lowerCasedCharacters = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z'
+];
+
+// Array of uppercase characters 
+var upperCasedCharacters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z'
+];
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+  if (generateBtn) {
+  var useLowerCase = prompt("Do you want to include lowercase characters?")
+
+var useUpperCase = prompt("Do you want to include uppercase characters?")
+
+var useNumeric = prompt("Do you want to include special characters?")
+
+useSpecial = prompt("Do you want to include number characters?")
+}
+let passwordLength=parseInt(prompt("How many characters do you want the password to be? The password cannot be less than 8 or more than 128 characters."));
+while(passwordLength < 8 || passwordLength > 128 || typeof(passwordLength) != "number" || passwordLength === NaN || passwordLength === null) {
+    alert("Please choose a number that is more than 8 and less than 128 characters");
+    passwordLength=parseInt(prompt("How many characters do you want the password to be? The password cannot be less than 8 or more than 128 characters."));
+}
+
+if (useLowerCase==true){
+  caseArray.push(lowerCaseChar);
+
+}
+if (useUpperCase==true){
+  caseArray.push(upperCaseChar)
+
+}
+if (useNumeric==true){
+  caseArray.push(numericChar)
+  
+}
+if (useSpecial==true){
+  caseArray.push(specialChar)
+}
+for(let i=0;i<passwordLength;i++){
+
+  let randomCharArrayNum;
+  let selectedCharArray;
+  let randomCharNum;
+  let randomChar;
+
+  //// a random number is generated for the group array (length of group array)
+  
+  randomCharArrayNum= parseInt(Math.floor(Math.random()*caseArray.length)); //random number based on length of case array
+  
+  //// a random number is generated for the character array (length of character array)
+
+  selectedCharArray=caseArray[randomCharArrayNum]; //variable that stores selected array from case array
+  
+  //console.log("The array has " + caseArray.length + " entries"); //testing length of caseArray
+  //console.log(randomCharArrayNum); //testing random number generated based on length of caseArray
+
+  randomCharNum=Math.floor(Math.random()*selectedCharArray.length);// variable that stores random number based on length of selected array
+
+  // console.log("The selected Array is the " + selectedCharArray); //testing array chosen
+  // console.log("The selected random index is " + randomCharNum); //testing random number generation based on chosen array length 
+
+  randomChar=selectedCharArray[randomCharNum];
+  //console.log(randomChar);
+  // console.log("Your random character array num is " + randomCharArrayNum + " and your random character number is " + randomCharNum);
+
+  // now, need to accumulate the string
+  passwordString+=randomChar;
+  //return passwordString;
+  
+}
+//console.log("Your password is " + passwordString);
+//alert("Your password is " + passwordString);
+passwordEntry.textContent=passwordString;    
+}
+
+// WHEN the password is generated
+// THEN the password is either displayed in an alert or written to the page
+
